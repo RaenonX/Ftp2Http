72ca605fa8eab9923a403c0b295cd1b9338cb1a8
RaenonX <raenonx0710@gmail.com>
2020-09-14 06:47:55 -0500

    FIX - logger configs
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 import logging
+from logging.config import dictConfig
 
 __all__ = ("apply_log_config", "log_exception")
 
@@ -6,11 +7,32 @@ from typing import Optional
 
 
 def apply_log_config():
-    logging.basicConfig(
-        filename="logs/console.log",
-        level=logging.DEBUG,
-        format="[%(asctime)s] %(levelname)s in %(module)s: %(message)s"
-    )
+    dictConfig({
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://flask.logging.wsgi_errors_stream",
+                "formatter": "default"
+            },
+            "console_handler": {
+                "level": "INFO",
+                "class": "logging.FileHandler",
+                "filename": "logs/console.log",
+                "formatter": "default",
+                "mode": "a"
+            }
+        },
+        "root": {
+            "level": "DEBUG",
+            "handlers": ["wsgi", "console_handler"]
+        }
+    })
 
 
 def log_exception(message: Optional[str] = None):
